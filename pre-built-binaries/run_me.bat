491264dae81f8543795da6bad69a13f06489899f
Sergey Ivanov <voivoid@mail.ru>
2018-11-01 13:19:08 +0300

    rename dir with test inputs

--- pre-built-binaries/run_me.bat
+++
@@ -1,8 +0,0 @@
-ShapeApp.exe<square1
-ShapeApp.exe<square2
-ShapeApp.exe<circle1
-ShapeApp.exe<circle2
-ShapeApp.exe<non_solid_square
-ShapeApp.exe<non_solid_circle
-ShapeApp.exe<rect
-ShapeApp.exe<rotated_square
